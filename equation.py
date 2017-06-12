9095a0b987e81bc96f6d3799bbbe4634c240c69f
haveswing <mr.osbourne@gmail.com>
2017-06-12 12:01:56 +0200

    dev.update

--- equation.py
+++ equation.py
@@ -57,12 +57,23 @@ print(math.floor(N))
 
 # build graphical simulation steps:
 
-v1 = 0
-v2 = 0
-v3 = 0
-v4 = 0
-v5 = 0
-v6 = 0
-v7 = 0
-vF = 0
+switchgraph = True
+
+if switchgraph:
+    v1 = 0
+    print(v1)
+    v2 = 0
+    print(v2)
+    v3 = 0
+    print(v3)
+    v4 = 0
+    print(v4)
+    v5 = 0
+    print(v5)
+    v6 = 0
+    print(v6)
+    v7 = 0
+    print(v7)
+    vF = 0
+    print(vF)
 
